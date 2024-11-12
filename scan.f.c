e982faa9d29803ae601bec61254e397dd4292a2c
valentinartempesta <v.tempesta@yahoo.com>
2024-11-12 10:46:21 -0500

    Esercizio 12.22

---
+++ scan.f.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+    int primo;
+    int secondo;
+    int risultato;
+
+    printf("Inserisci primo numero");
+    scanf("%d , &primo");
+}
+
+
+
---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+* Questo è il mio codice 
+*/
+
+// anche questo è un commento 
+
+int main()
+{
+    printf("ciao mondo!");
+    return 0;
+}
+
+
+
